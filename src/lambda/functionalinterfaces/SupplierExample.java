3e854a13118c6129a7462a5adc78d304b35b6435
ykucukcinar <yigitkucukcinar@hotmail.com>
2025-03-29 00:35:23 +0300

    add supplier example

---
+++ src/lambda/functionalinterfaces/SupplierExample.java
@@ -0,0 +1,10 @@
+package lambda.functionalinterfaces;
+
+import java.util.function.Supplier;
+
+public class SupplierExample {
+    public static void main(String[] args) {
+        Supplier<Double> randomSupplier = () -> Math.random();
+        System.out.println(randomSupplier.get());
+    }
+}
