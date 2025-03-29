d4961bebca95068e51fd56e2e9a77d77e81cdb24
ykucukcinar <yigitkucukcinar@hotmail.com>
2025-03-30 00:16:42 +0300

    add notes and method reference examples

---
+++ src/lambda/methodreferences/Methodreferences.java
@@ -0,0 +1,20 @@
+package lambda.methodreferences;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class Methodreferences {
+    public static void main(String[] args) {
+        // Lambda: str -> System.out.println();
+        Consumer<String> print = System.out::println;
+
+        // Lambda: () -> new ArrayList();
+        Supplier<List<String>> listSupplier = ArrayList::new;
+
+        // Lambda: str -> str.toUpperCase;
+        Function<String, String> toUpper = String::toUpperCase;
+    }
+}
