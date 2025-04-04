5f1c8251157bab021aba0ed751a29289b923bb54
ykucukcinar <yigitkucukcinar@hotmail.com>
2025-04-04 22:05:27 +0300

    add queue examples

---
+++ src/collections/queue/QueueExample.java
@@ -0,0 +1,23 @@
+package collections.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        Queue<String> tasks = new LinkedList<>();
+
+        tasks.offer("Breakfast"); // adds to queue, if success returns true
+        tasks.offer("Meeting");
+        tasks.add("Coding"); // if capacity is full, an exception will be thrown
+
+        System.out.println("peek(): " + tasks.peek()); // Returns first elements, if empty returns false
+        System.out.println("element(): " + tasks.element()); // Returns first elements, if empty NoSuchElementException be thrown
+
+        System.out.println("poll(): " + tasks.poll()); // Removes first element, if empty returns false
+        System.out.println("remove(): " + tasks.remove()); // Removes first element, if empty Exception
+
+        System.out.println("Remainings: " + tasks);
+
+    }
+}
