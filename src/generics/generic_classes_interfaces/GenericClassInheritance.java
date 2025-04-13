16d564781c5d7e6cca44cb17ffad50496d237ea0
ykucukcinar <yigitkucukcinar@hotmail.com>
2025-04-14 00:34:18 +0300

    add generic classes and interfaces examples

---
+++ src/generics/generic_classes_interfaces/GenericClassInheritance.java
@@ -0,0 +1,43 @@
+package generics.generic_classes_interfaces;
+
+class Container<T> {
+    T content;
+
+    public void set(T content) {
+        this.content = content;
+    }
+
+    public T get() {
+        return content;
+    }
+}
+
+// Concrete subclass
+class StringContainer extends Container<String> {
+    public void shout() {
+        System.out.println("Shouting: " + get().toUpperCase());
+    }
+}
+
+// Generic subclass
+class LoggingContainer<T> extends Container<T> {
+    @Override
+    public void set(T content) {
+        System.out.println("Setting value: " + content);
+        super.set(content);
+    }
+}
+
+public class GenericClassInheritance {
+    public static void main(String[] args) {
+        StringContainer sc = new StringContainer();
+        sc.set("hello");
+        sc.shout();
+
+        LoggingContainer<Integer> lc = new LoggingContainer<>();
+        lc.set(123);
+        System.out.println("Logged value: " + lc.get());
+    }
+
+    // 🔹 You can specialize a generic class by fixing its type in the subclass, or extend it generically.
+}
